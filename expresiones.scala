50d9d969d1f2e153163c800cdc07e3c66236bbf9
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2024-02-13 04:32:20 +0000

    Realizo una practica de las expresiones en Scala

---
+++ expresiones.scala
@@ -0,0 +1,12 @@
+//La mayoria de las expresiones retornan algo, una definicion de no retornar nada es declarando retorno unit
+
+def x = (x) //Esta forma de definir solo admite una sentencia
+
+def x = 3
+
+def x = {2;3} //Esta forma admite varias sentencias
+
+//Ejemplos de uso
+
+if (x > 3) "el numero es mayor que 3" else "el numero es menor o igual a 3"
+
